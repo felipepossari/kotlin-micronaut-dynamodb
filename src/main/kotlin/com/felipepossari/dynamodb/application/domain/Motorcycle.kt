e6606d7fbd138e45488ece02cfcc79d97af5d83d
Felipe Possari <fcpfelipe@hotmail.com>
2021-03-05 08:43:19 -0300

    Add new domain class motorcycle

---
+++ src/main/kotlin/com/felipepossari/dynamodb/application/domain/Motorcycle.kt
@@ -0,0 +1,11 @@
+package com.felipepossari.dynamodb.application.domain
+
+class Motorcycle(
+        val id: String = "",
+        val brand: String = "",
+        val model: String = "",
+        val manufactureYear: Int = 0,
+        val modelYear: Int = 0,
+        val color: String = ""
+) {
+}
