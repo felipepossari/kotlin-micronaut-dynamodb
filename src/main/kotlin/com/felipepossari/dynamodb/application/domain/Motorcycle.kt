d5afa15020ea1c8358aaf7beee7d22d42cce1f7b
Felipe Possari <fcpfelipe@hotmail.com>
2021-03-08 06:43:49 -0300

    Domain field validation added

--- src/main/kotlin/com/felipepossari/dynamodb/application/domain/Motorcycle.kt
+++ src/main/kotlin/com/felipepossari/dynamodb/application/domain/Motorcycle.kt
@@ -1,5 +1,9 @@
 package com.felipepossari.dynamodb.application.domain
 
+import com.felipepossari.dynamodb.application.exception.InvalidDomainFieldException
+import com.felipepossari.dynamodb.application.exception.model.ErrorReason
+import java.util.*
+
 data class Motorcycle(
         val id: String = "",
         val brand: String = "",
@@ -8,4 +12,85 @@ data class Motorcycle(
         val modelYear: Int = 0,
         val color: String = ""
 ) {
+    companion object {
+        private const val BRAND_MAX_LENGTH: Int = 256
+        private const val MODEL_MAX_LENGTH: Int = 256
+        private const val COLOR_MAX_LENGTH: Int = 256
+    }
+
+    fun validateFieldsForCreate() {
+        var errors = mutableListOf<ErrorReason>()
+
+        validateBrand(brand)?.let { errors.add(it) }
+        validateModel(model)?.let { errors.add(it) }
+        validateManufactureYear(manufactureYear)?.let { errors.add(it) }
+        validateModelYear(modelYear)?.let { errors.add(it) }
+        validateColor(color)?.let { errors.add(it) }
+
+        if (errors.isNotEmpty()) {
+            throw InvalidDomainFieldException(errors)
+        }
+    }
+
+    fun validateFieldsForUpdate() {
+        var errors = mutableListOf<ErrorReason>()
+        validateId(id)?.let { errors.add(it) }
+        validateBrand(brand)?.let { errors.add(it) }
+        validateModel(model)?.let { errors.add(it) }
+        validateManufactureYear(manufactureYear)?.let { errors.add(it) }
+        validateModelYear(modelYear)?.let { errors.add(it) }
+        validateColor(color)?.let { errors.add(it) }
+
+        if (errors.isNotEmpty()) {
+            throw InvalidDomainFieldException(errors)
+        }
+    }
+
+    private fun validateId(id: String): ErrorReason? {
+        if (id.isEmpty()) {
+            return ErrorReason.ID_INVALID
+        } else {
+            try {
+                UUID.fromString(id)
+            } catch (ex: IllegalArgumentException) {
+                return ErrorReason.ID_INVALID
+            }
+        }
+        return null
+    }
+
+    private fun validateBrand(brand: String): ErrorReason? =
+            if (brand.isEmpty() || brand.length >= BRAND_MAX_LENGTH) {
+                ErrorReason.BRAND_INVALID
+            } else {
+                null
+            }
+
+    private fun validateModel(model: String): ErrorReason? =
+            if (model.isEmpty() || model.length >= MODEL_MAX_LENGTH) {
+                ErrorReason.MODEL_INVALID
+            } else {
+                null
+            }
+
+    private fun validateManufactureYear(manufactureYear: Int): ErrorReason? =
+            if (manufactureYear <= 0) {
+                ErrorReason.MANUFACTURE_YEAR_INVALID
+            } else {
+                null
+            }
+
+    private fun validateModelYear(modelYear: Int): ErrorReason? =
+            if (modelYear <= 0) {
+                ErrorReason.MODEL_YEAR_INVALID
+            } else {
+                null
+            }
+
+    private fun validateColor(color: String): ErrorReason? =
+            if (color.isEmpty() || color.length >= COLOR_MAX_LENGTH) {
+                ErrorReason.COLOR_INVALID
+            } else {
+                null
+            }
 }
--- src/main/kotlin/com/felipepossari/dynamodb/application/exception/model/ErrorReason.kt
+++ src/main/kotlin/com/felipepossari/dynamodb/application/exception/model/ErrorReason.kt
@@ -7,7 +7,13 @@ enum class ErrorReason(val code: String, val message: String) {
     PHONE_INVALID("003", "Phone cannot be null or bigger than 25 characters"),
     PASSWORD_INVALID("004", "Password cannot be null"),
 
-    RESOURCE_ALREADY_EXISTS("010", "Resource already exists"),
-    RESOURCE_NOT_FOUND("011", "Resource not found"),
-    EMAIL_ALREADY_USED("012", "Email already used by another resource")
+    ID_INVALID("005", "Id cannot be null or invalid"),
+    BRAND_INVALID("006", "Brand cannot be null or bigger than 255 characters"),
+    MODEL_INVALID("007", "Model cannot be null or bigger than 255 characters"),
+    MANUFACTURE_YEAR_INVALID("008", "Manufacture year invalid"),
+    MODEL_YEAR_INVALID("009", "Manufacture year invalid"),
+    COLOR_INVALID("010", "Brand cannot be null or bigger than 255 characters"),
+
+    RESOURCE_ALREADY_EXISTS("011", "Resource already exists"),
+    RESOURCE_NOT_FOUND("012", "Resource not found")
 }
